b76f0c85c15914d03c9d61cd183e364d2fd2d8d5
Lucas Ellenberger <lmellenb@ucsc.edu>
2025-08-08 10:36:02 -0700

    Factored out login user logic so it can be exported.

--- site/js/modules/webui/login.test.js
+++ site/js/modules/webui/login.test.js
@@ -9,45 +9,54 @@ test("Login Page", async function() {
     Base.init(false);
 
     const testCases = [
-        { email: "course-admin@test.edulinq.org", password: "course-admin" },
-        { email: "course-grader@test.edulinq.org", password: "course-grader" },
-        { email: "course-other@test.edulinq.org", password: "course-other" },
-        { email: "course-owner@test.edulinq.org", password: "course-owner" },
-        { email: "course-student@test.edulinq.org", password: "course-student" },
+        { displayName: "course-admin" },
+        { displayName: "course-grader" },
+        { displayName: "course-other" },
+        { displayName: "course-owner" },
+        { displayName: "course-student" },
 
-        { email: "server-admin@test.edulinq.org", password: "server-admin" },
-        { email: "server-creator@test.edulinq.org", password: "server-creator" },
-        { email: "server-owner@test.edulinq.org", password: "server-owner" },
-        { email: "server-user@test.edulinq.org", password: "server-user" },
+        { displayName: "server-admin" },
+        { displayName: "server-creator" },
+        { displayName: "server-owner" },
+        { displayName: "server-user" },
     ];
 
-    for (const { email, password } of testCases) {
-        // Do not send an API request to delete the credentials.
-        // The API test data does not contain token deletion data.
-        Autograder.clearCredentials(false);
-        Context.clear();
+    for (const { displayName } of testCases) {
+        loginUser(displayName)
+            .then(function() {
+                expect(document.title).toContain("Home");
 
-        let changedToLogin = TestUtil.waitForDOMChange('.page-body .content[data-page="login"]');
-        Routing.redirectLogin();
-        await changedToLogin;
+                let currentUserSpan = document.querySelector('.current-user span');
+                expect(currentUserSpan).not.toBeNull();
 
-        // Fill out the user info.
-        let emailField = document.querySelector(`.user-input-fields .input-field[data-name="email"] input`);
-        let passField = document.querySelector(`.user-input-fields .input-field[data-name="cleartext"] input`);
-
-        emailField.value = email;
-        passField.value = password;
-
-        let loginPromise = TestUtil.waitForDOMChange('.page-body .content[data-page="home"]');
-        document.querySelector('.template-button').click();
-        await loginPromise;
-
-        expect(document.title).toContain("Home");
-
-        let currentUserSpan = document.querySelector('.current-user span');
-        expect(currentUserSpan).not.toBeNull();
-
-        let displayName = password;
-        expect(currentUserSpan.textContent).toContain(displayName);
+                expect(currentUserSpan.textContent).toContain(displayName);
+            })
+        ;
     }
 });
+
+async function loginUser(displayName) {
+    // Do not send an API request to delete the credentials.
+    // The API test data does not contain token deletion data.
+    Autograder.clearCredentials(false);
+    Context.clear();
+
+    let changedToLogin = TestUtil.waitForDOMChange('.page-body .content[data-page="login"]');
+    Routing.redirectLogin();
+    await changedToLogin;
+
+    // Fill out the user info.
+    let emailField = document.querySelector(`.user-input-fields .input-field[data-name="email"] input`);
+    let passField = document.querySelector(`.user-input-fields .input-field[data-name="cleartext"] input`);
+
+    emailField.value = `${displayName}@test.edulinq.org`;
+    passField.value = displayName;
+
+    let loginPromise = TestUtil.waitForDOMChange('.page-body .content[data-page="home"]');
+    document.querySelector('.template-button').click();
+    await loginPromise;
+}
+
+export {
+    loginUser,
+}
