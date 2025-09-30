69452372f2a5d33369e3a97cc7f29b07ab2e31f3
Lucas Ellenberger <lmellenb@ucsc.edu>
2025-09-30 15:33:08 -0700

    (wip) Adding a waiting period to the screenshot script.

--- scripts/generate_screenshots.py
+++ scripts/generate_screenshots.py
@@ -1,11 +1,21 @@
 #!/usr/bin/env python3
 
 import os
+import requests
 import selenium.webdriver
 import sys
+import time
 
 THIS_DIR = os.path.abspath(os.path.dirname(os.path.realpath(__file__)))
 RESOURCES_DIR = os.path.abspath(os.path.join(THIS_DIR, '..', 'resources'))
+
+DEFAULT_SITE = 'http://localhost'
+DEFAULT_PORT = 8080
+DEFAULT_START_URL = '%s:%s' % (DEFAULT_SITE, DEFAULT_PORT)
+
+DEFAULT_REQUEST_RETRIES = 10
+DEFAULT_REQUEST_TIMEOUT = 2
+DEFAULT_REQUEST_SLEEP_TIME = 3
 
 def make_output_path(image_name, light_mode = True):
     mode_name = 'light'
@@ -14,6 +24,22 @@ def make_output_path(image_name, light_mode = True):
 
     file_name = '%s-%s.png' % (image_name, mode_name)
     return os.path.join(RESOURCES_DIR, file_name)
+
+def wait_for_server_start(start_url = DEFAULT_START_URL, num_retries = DEFAULT_REQUEST_RETRIES, request_timeout = DEFAULT_REQUEST_TIMEOUT, sleep_time = DEFAULT_REQUEST_SLEEP_TIME):
+    for _ in range(num_retries):
+        try:
+            response = requests.get(start_url, timeout = request_timeout)
+            if response.status_code == 200:
+                print("Server is up!")
+                return
+        except requests.exceptions.RequestException:
+            pass
+
+        print("Waiting for server...")
+        time.sleep(sleep_time)
+
+    # Server failed to start.
+    raise RuntimeError("Server did not start in time.")
 
 def main():
     chrome_options = selenium.webdriver.chrome.options.Options()
@@ -28,8 +54,9 @@ def main():
 
     driver = selenium.webdriver.Chrome(options=chrome_options)
 
-    start_url = 'http://localhost:8080'
-    driver.get(start_url)
+    wait_for_server_start()
+
+    driver.get(DEFAULT_START_URL)
     driver.implicitly_wait(2)
 
     driver.save_screenshot(make_output_path('login'))
